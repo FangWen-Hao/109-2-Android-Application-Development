03d66f2eb3c2727417478b41fb8c4f96998092d5
FangWen-Hao <t108590048@ntut.org.tw>
2021-03-01 11:52:00 +0800

    HW01

---
+++ HW1/108590048_HW1/app/src/main/java/com/example/hw1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.hw1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Testing log.d for HW1");
+        Log.e("MainActivity", "Testing log.e for HW1");
+    }
+}
